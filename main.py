e166005d660c709c67b3c43a3d08f463d8577016
adventureloop <jones@sdf.org>
2016-11-03 20:25:34 +0000

    Bring in some code for all over the shop

--- main.py
+++ main.py
@@ -1,48 +1,100 @@
-#!/usr/bin/env python
+### Author: tj <tj@enoti.me>
+### Description: Scottish Consulate Techno Orbital Tracker
+### Category: Other
+### License: BSD 3
 
+import sys
+import os
+import time
 import socket
+import ugfx
+import buttons
+import wifi
 
-TCP_IP = '127.0.0.1'
-TCP_PORT = 4533
-BUFFER_SIZE = 100
+PORT = 4533
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((TCP_IP, TCP_PORT))
-s.listen(1)
+container = None
+textcontainer = None
 
-conn, addr = s.accept()
-print 'Connection address:', addr
+target_az = 0.0
+target_el = 0.0
 
-az = 0.0
-el = 0.0
+current_az = 0.0
+current_el = 0.0
 
-while 1:
-    data = conn.recv(BUFFER_SIZE)
-    if not data:
-        break
+def render(data):
+    print("drawing")
 
-    print("received data:", data)
+def processbuttons(data):
+    #buttons.is_pressed("JOY_RIGHT"):
+    #buttons.is_pressed("JOY_LEFT"):
+    #buttons.is_pressed("JOY_DOWN"):
+    #buttons.is_pressed("JOY_UP"):
+    #buttons.is_pressed("JOY_CENTER"):
+    #buttons.is_pressed("BTN_A"):
+    #buttons.is_pressed("BTN_B"):
+    #buttons.is_pressed("BTN_MENU"):
 
-    if data == "p\n":
-        print("pos query at az:{} el: {}", az, el);
+    print("readings buttons")
 
-        response = "{}\n{}\n".format(az, el)
-        print("responing with: \n {}".format(response))
-        conn.send(response)
-    elif data.startswith("P "):
-        values = data.split("  ")
-        print(values)
-        az = float(values[1])
-        el = float(values[2])
+def drawtext(con):
+    con.clear()
+    con.set_default_font(ugfx.FONT_NAME)
+    con.text(0, 5, "SATTRACKER", ugfx.GREEN)
+    
+def sattracker():
 
-        print("moving to az:{} el: {}".format( az, el));
+    print("following a satellite")
+    while True:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.bind((TCP_IP, TCP_PORT))
+        s.listen(1)
 
-        conn.send(" ")
-    elif data == "q\n":
-        print("close command, shutting down")
-        conn.close()
-        exit()
-    else:
-        print("unknown command, closing socket")
-        conn.close()
-        exit()
+        conn, addr = s.accept()
+        print 'Connection address:', addr
+
+        data = conn.recv(BUFFER_SIZE)
+        if not data:
+            break
+
+        print("received data:", data)
+
+        if data == "p\n":
+            print("pos query at az:{} el: {}", az, el);
+
+            response = "{}\n{}\n".format(az, el)
+            print("responing with: \n {}".format(response))
+            conn.send(response)
+        elif data.startswith("P "):
+            values = data.split("  ")
+            print(values)
+            az = float(values[1])
+            el = float(values[2])
+
+            print("moving to az:{} el: {}".format( az, el));
+
+            conn.send(" ")
+        elif data == "q\n":
+            print("close command, shutting down")
+            conn.close()
+            break
+        else:
+            print("unknown command, closing socket")
+            conn.close()
+            break
+
+
+#if __name__ == "__main__":
+print("starting")
+
+buttons.init()
+ugfx.init()
+ugfx.clear(ugfx.BLACK)
+
+textcontainer = ugfx.Container(0, 0, 320, 80)
+container = ugfx.Container(0, 80,320,160)
+
+drawtext(textcontainer)
+wifi.connect()
+
+sattracker()
