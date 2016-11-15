478f246f4b09f689d4619b2c381b4cf91756f287
tj <tj@enoti.me>
2016-11-15 22:51:09 +0000

    Working rot controller, controllable from gpredict

--- main.py
+++ main.py
@@ -23,8 +23,8 @@ target_el = 0.0
 current_az = 0.0
 current_el = 0.0
 
-def render(data):
-    print("drawing")
+az = None
+el = None
 
 def processbuttons(data):
     #buttons.is_pressed("JOY_RIGHT"):
@@ -38,13 +38,21 @@ def processbuttons(data):
 
     print("readings buttons")
 
-def drawtext(con, status="searching for long range comms..."):
-    con.clear()
-    con.set_default_font(ugfx.FONT_NAME)
-    con.text(0, 5, "SATTRACKER", ugfx.GREEN)
+def drawtext(status="Searching for long range comms..."):
+    global current_az
+    global current_el
 
-    con.set_default_font(ugfx.FONT_SMALL)
-    con.text(0, 50, status, ugfx.GREEN)
+    ugfx.clear(ugfx.BLACK)
+    ugfx.set_default_font(ugfx.FONT_NAME)
+    ugfx.text(0, 5, "SATTRACKER", ugfx.GREEN)
+
+    ugfx.set_default_font(ugfx.FONT_SMALL)
+    ugfx.text(0, 50, status, ugfx.RED)
+
+
+    posstring = "AZ: {} EL: {}".format(current_az, current_el)
+    ugfx.set_default_font(ugfx.FONT_SMALL)
+    ugfx.text(0, 70, posstring, ugfx.YELLOW)
 
 #
 #  ___ ___ _ ___ _____ ___
@@ -52,45 +60,16 @@ def drawtext(con, status="searching for long range comms..."):
 # /__/\___|_|  \_/\___/__/
 #
 #
-# json:
-servoconfig = """
-{
-    "el_servo":
-    [
-        {
-            "pin":"1",
-            "limits":
-            [
-                "0",
-                "180"
-            ]
-        }
-    ]
-    "az_servo":
-    [
-        {
-            "pin":"2",
-            "limits":
-            [
-                "0",
-                "180"
-            ]
-        }
-        {
-            "pin":"4",
-            "limits":
-            [
-                "0",
-                "180"
-            ]
-        }
-    ]
-}
-"""
-
-
 def calibrateservos():
-    print("I am a servo")
+
+    global az
+    global el
+
+    az = pyb.Servo(1)
+    el = pyb.Servo(2)
+
+    az.angle(0)
+    el.angle(0)
 
 #           _               _           
 #  ___ __ _| |_ _ _ __ _ __| |_____ _ _ 
@@ -99,46 +78,71 @@ def calibrateservos():
 #
 #
 def sattracker():
+
+    global current_az
+    global current_el
+
+    global target_az
+    global target_el 
+
+    calibrateservos()
+    
     print("following a satellite")
+
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    socket.settimeout(0.5)   #socket will block for most 0.5 seconds
+
     s.bind((ANY_ADDR, PORT))
     s.listen(1)
-    while True:
-        sleep(0.1)           #screen will update at most 10Hz
+    s.settimeout(0.5)   #socket will block for most 0.5 seconds
 
+    connected = False
+
+    while True:
+        #time.sleep(0.1)           #screen will update at most 10Hz
+
+        status = "waiting for connection"
         conn, addr = s.accept()
-        print 'Connection from:', addr
+        if conn:
+            status = 'Connection from {}:{}'.format(addr[0], addr[1])
+            drawtext(status)
+            connected = True
+        else:
+            continue
 
         while connected:
-            data = conn.recv(BUFFER_SIZE)
+            data = conn.recv(100).decode("utf-8") #timeouts here aren't handled
+
             if not data:
                 break
 
-            print("received data:", data)
-
             if data == "p\n":
-                print("pos query when at az:{} el: {}", current_az, current_el);
-
-                response = "{}\n{}\n".format(az, el)
-                print("responing with: \n {}".format(response))
+                response = "{}\n{}\n".format(current_az, current_el)
                 conn.send(response)
             elif data.startswith("P "):
                 values = data.split("  ")
-                print(values)
+                if len(values) != 3:
+                    continue
+
                 target_az = float(values[1])
                 target_el = float(values[2])
-
-                print("moving to az:{} el: {}".format(target_az, target_el));
-
                 conn.send(" ")
+
+                az.angle(target_az)
+                el.angle(target_el)
+
+                current_az = target_az
+                current_el = target_el
+
+                drawtext(status)
             elif data == "q\n":
                 print("close command, shutting down")
                 conn.close()
+                connected = False
                 break
             else:
                 print("unknown command, closing socket")
                 conn.close()
+                connected = False
                 break
 
 #if __name__ == "__main__":
@@ -151,7 +155,7 @@ ugfx.clear(ugfx.BLACK)
 textcontainer = ugfx.Container(0, 0, 320, 80)
 container = ugfx.Container(0, 80,320,160)
 
-drawtext(textcontainer)
+drawtext()
 wifi.connect()
 
 sattracker()
